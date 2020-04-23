0de0faccde8d9e36e3b23eaa6e9c17f5a95269db
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-23 11:43:38 +0100

    got it working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let speech;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	noCanvas();
+	speech=new p5.Speech(voiceReady);
+	function voiceReady() {
+		console.log(speech.voices);
+	}
+	//createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
+//function draw() {
+	//rect(10,10,100,100);
+//}
 
+function mousePressed() {
+	speech.setVoice('Xander');
+	speech.speak('Steak.');
 }
