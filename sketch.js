c24453f5f309f787f3b31bfa01ad95f20149234f
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-23 16:45:13 +0100

    added controls

--- sketch.js
+++ sketch.js
@@ -1,19 +1,33 @@
 let speech;
+var inputwords;
+var btnLoadWords;
+var txtenterword;
 
 function setup() {
 	noCanvas();
-	speech=new p5.Speech(voiceReady);
+	speech = new p5.Speech(voiceReady);
+
 	function voiceReady() {
 		console.log(speech.voices);
 	}
-	//createCanvas(windowWidth, windowHeight);
+	//speech.setVoice('Xander');
+	inputwords = createElement("textarea", "hello");
+	createElement("br");
+	btnLoadWords = createButton("Load Words");
+	inputwords.elt.rows = 20;
+	inputwords.elt.cols = 10;
+	btnLoadWords.mousePressed(loadWords);
 }
 
-//function draw() {
-	//rect(10,10,100,100);
-//}
+function loadWords() {
+	inputwords.hide();
+	btnLoadWords.hide();
+	createElement('h2', 'Study the spelling of this word.');
+	txtenterword=createInput();
+	txtenterword.focused=true;
+}
 
 function mousePressed() {
-	speech.setVoice('Xander');
-	speech.speak('Steak.');
+	
+	//speech.speak('Steak.');
 }
